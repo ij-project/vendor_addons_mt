e6abe103b3e06fe50c820c07825e0b5f2149906a
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-11-13 17:19:33 +0600

    Drop some navbar style

--- config.mk
+++ config.mk
@@ -223,12 +223,9 @@ PRODUCT_PACKAGES += \
 
 # Navbar styles
 PRODUCT_PACKAGES += \
-    NavbarAndroidOverlay \
-	NavbarAsusOverlay \
-	NavbarDoraOverlay \
+    NavbarAsusOverlay \
     NavbarMotoOverlay \
     NavbarNexusOverlay \
-    NavbarOldOverlay \
     NavbarOnePlusOverlay \
     NavbarOneUiOverlay \
     NavbarSammyOverlay \
