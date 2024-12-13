4320a8260dd83c8220bbbc10fa61c4689296e309
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-13 09:00:24 +0800

    Add power menu themes overlays
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- config.mk
+++ config.mk
@@ -317,6 +317,13 @@ PRODUCT_PACKAGES += \
     NotifIOS \
     NotifLayers
 
+# Power Menu Themes
+PRODUCT_PACKAGES += \
+    PowerCyberPunk \
+    PowerDuoline \
+    PowerIOS \
+    PowerLayers
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
