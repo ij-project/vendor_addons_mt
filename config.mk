ecc8fa2dd263af3f58adfec61e469bb615278f95
Pranav Vashi <neobuddy89@gmail.com>
2025-03-01 02:09:23 +0530

    addons: Remove current navigation bar overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -217,9 +217,7 @@ PRODUCT_PACKAGES += \
 # Navbar
 PRODUCT_PACKAGES += \
     GesturalNavigationOverlayLong \
-    GesturalNavigationOverlayMedium \
-    GesturalNavigationOverlayHidden \
-    GesturalNavigationOverlayHiddenNarrow
+    GesturalNavigationOverlayMedium
 
 # Navbar styles
 PRODUCT_PACKAGES += \
